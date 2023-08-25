8143256a9a5f19c586cb9090dadc7d0a1d9a4ca7
Franco Griffone <pipokn@gmail.com>
2023-08-25 16:29:36 -0300

    datepickers + navbar + bootstrap + imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//MATERIAL UI
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatDialogModule } from '@angular/material/dialog';
+//FORMS
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FichadorComponent } from './components/fichador/fichador.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FichadorComponent
+    FichadorComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/fichador/fichador.component.ts
+++ src/app/components/fichador/fichador.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-fichador',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./fichador.component.scss']
 })
 export class FichadorComponent {
+
+  fecha = new Date()
+
+  subtractDay(date: Date, day: number) {
+    date.setDate(date.getDate() - day);
+    return date;
+  }
+
+  fechaBusqueda: any = this.subtractDay(this.fecha, 1).toISOString().slice(0,-14)
+
+  fechaInicio = new FormControl(this.fechaBusqueda);
+  fechaFinal = new FormControl(this.fecha);
+
 
 }
